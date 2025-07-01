8277617076f66b0ce6ab9ca255235c4dda634677
Matheus Cunha <matheuscunhaprado@gmail.com>
2025-06-30 21:26:05 -0300

    Configuring Cross-Origin Resource Sharing (CORS)

---
+++ 13-ConfiguringCORS/rest-with-spring-boot-and-java/src/main/java/com/github/MathCunha16/controllers/BookController.java
@@ -0,0 +1,74 @@
+package com.github.MathCunha16.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.MathCunha16.controllers.docs.BookControllerDocs;
+import com.github.MathCunha16.data.dto.BookDTO;
+import com.github.MathCunha16.services.BookServices;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/book/v1")
+@Tag(name = "Books", description = ("Endpoints For Managing Books"))
+public class BookController implements BookControllerDocs {
+
+	@Autowired
+	BookServices bookServices;
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_YAML_VALUE })
+	@Override
+	public List<BookDTO> findAll() {
+
+		return bookServices.findAll();
+	}
+
+	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_YAML_VALUE })
+	@Override
+	public BookDTO findById(@PathVariable("id") Integer id) {
+
+		return bookServices.findById(id);
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_YAML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+	@Override
+	public BookDTO create(@RequestBody BookDTO book) {
+
+		return bookServices.create(book);
+	}
+
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_YAML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+	@Override
+	public BookDTO update(@RequestBody BookDTO book) {
+
+		return bookServices.update(book);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@Override
+	public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+
+		bookServices.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
+}
---
+++ 13-ConfiguringCORS/rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES 
+(1, 'Matheus', 'Prado', 'Goiânia - Brasil', 'male'),
+(2, 'Neymar', 'Junior', 'Santos - Brasil', 'male'),
+(3, 'Ribamar', 'Ferreira', 'São Paulo - Brasil', 'male'),
+(4, 'Marcio', 'Marciano', 'São Paulo - Brasil', 'male');
